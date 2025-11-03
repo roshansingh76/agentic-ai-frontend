61d9955e344d2ee24f24c807cf358c84e9c9ea77
roshan singh <roshansingh9450@gmail.com>
2025-11-03 23:10:41 +0530

    react code update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/react/',
   server: { port: 3000, host: true },
   build: { outDir: 'dist' }
 })
